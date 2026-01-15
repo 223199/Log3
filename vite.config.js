63d17c001f4ea24fe680693e8e13187f770faadc
223199 <162702161+223199@users.noreply.github.com>
2026-01-15 13:12:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Log3],
+  plugins: [react()],
+  base: "/Log3/",   // ← これが超重要
+});
 })
